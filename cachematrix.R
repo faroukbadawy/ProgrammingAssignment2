7329477fdcd6d3f76a8bbca99e70d4924f5a4ba6
Elfarouk Badawy <farouk.badawy.public@gmail.com>
2017-01-20 04:11:21 -0800

    Assignment Done

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Following functions handles a cachable matrix operations
 
-## Write a short comment describing this function
+## Function to create special matrix object that is able to, set value, 
+## get value, set inverse & get inverse
+## Input: Square matrix
+## Output: List of functions to set, get, set inverse & get inverse
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mat = matrix()) {
+    ## Object to store cached matrix inverse
+    mat_inverse <- NULL
+    
+    ## Set matrix with new value, and resets cached matrix inverse
+    set <- function(y) {
+        mat <<- y
+        mat_inverse <<- NULL
+    }
+    
+    get <- function() mat
+    
+    setinverse <- function(inverse) mat_inverse <<- inverse
+    
+    getinverse <- function() mat_inverse
+    
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Returns cached matrix inverse if exists, or re-computes new inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Get cached matrix inverse
+    m <- x$getinverse()
+    
+    ## If matrix inverse was computed before, return cached value, otherwise
+    ## compute new inverse
+    if(!is.null(m)) {
+        message("Getting cached data")
+        return(m)
+    }
+    
+    ## Re-compute matrix inverse and store value in matrix inverse cache object
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
 }
